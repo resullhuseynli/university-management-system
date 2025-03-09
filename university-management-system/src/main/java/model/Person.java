ff0dc9fbff7b2a7273402bcf70ab32e2354a5802
Resul Huseynli <resulhuseynli4@gmail.com>
2025-03-09 12:24:10 +0400

    Person Created!

--- university-management-system/src/main/java/model/Person.java
+++ university-management-system/src/main/java/model/Person.java
@@ -1,7 +1,71 @@
 package model;
 
-public class Person {
-    public static void main(String[] args) {
+import java.util.Objects;
 
+public class Person {
+    private int id;
+    private String name;
+    private String surname;
+    private int age;
+
+    public Person(int id, String name, String surname, int age) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return id == person.id && age == person.age && Objects.equals(name, person.name) && Objects.equals(surname, person.surname);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, surname, age);
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", age=" + age +
+                '}';
     }
 }
